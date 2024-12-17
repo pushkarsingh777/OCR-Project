0d4d3c016acfff03c16be6e454127314fb648b93
Pushkar Singh <147143927+pushkarsingh777@users.noreply.github.com>
2024-12-17 20:33:42 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,13 +2,13 @@ import easyocr
 import json
 
 def extract_text(image_path):
-    # Initialize the EasyOCR Reader for English and Hindi
-    reader = easyocr.Reader(['en', 'hi'])  # You can add more languages if needed
+    
+    reader = easyocr.Reader(['en', 'hi']) 
 
-    # Perform OCR on the image
+    
     results = reader.readtext(image_path)
 
-    # Extract text from results
+    
     extracted_text = []
     for (bbox, text, prob) in results:
         extracted_text.append(text)
